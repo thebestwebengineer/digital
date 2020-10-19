c23c247caa243d61d7e343702865ae89e7d08454
theBESTwebENGINEER <kkwez0910@gmail.com>
2020-10-19 16:41:53 +0900

    DAY1 과제

---
+++ Java/src/day1/과제.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class 과제 {
+
+	public static void main(String[] args) {
+		/*
+		 * 필요한 프로그램 설치
+		 * GitKarken , github와 연동 (클론)
+		 * jdk, jre, eclipse  (테스트해볼것)
+		 * 
+		 * 다시처음부터 해볼것.
+		 */
+
+	}
+
+}
